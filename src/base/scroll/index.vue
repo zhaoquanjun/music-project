993c4d92528f5d9663bc9c5d28e19f2e65d3c736
zhaoquanjun <zhaoquanjun0@qq.com>
2018-03-15 10:49:24 +0800

    push

---
+++ src/base/scroll/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      data: {
+        type: Array,
+        default: null
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
+      },
+      pullUp: {
+        type: Boolean,
+        default: false
+      },
+      pulldown: {
+        type: Boolean,
+        default: false
+      },
+      beforeScroll: {
+        type: Boolean,
+        default: false
+      },
+      refreshDelay: {
+        type: Number,
+        default: 20
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this._initScroll()
+      })
+      // 与上面的是一样的效果，20是一个经验值，每个tick大约17ms
+      // setTimeout(function(){
+      //  this._initScroll()
+      // }, 20)   no-trailing-space
+    },
+    methods: {
+      _initScroll() {
+        if (!this.$refs.wrapper) {
+          return
+        }
+        // 初始化 better-scroll
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+
+        if (this.listenScroll) {
+          let _this = this
+          this.scroll.on('scroll', pos => {
+            // 派发事件获取pos 值
+            _this.$emit('scroll', pos)
+          })
+        }
+        // 上拉加载
+        if (this.pullUp) {
+          this.scroll.on('scrollEnd', () => {
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+              this.$emit('scrollToEnd')
+            }
+          })
+        }
+        // 下拉刷新
+        if (this.pulldown) {
+          this.scorll.on('touchend', (pos) => {
+            // 下拉动作
+            if (pos.y > 50) {
+              this.$emit('pulldown')
+            }
+          })
+        }
+        // 开始滚动前判断
+        if (this.beforeScroll) {
+          this.scroll.on('beforeScrollStart', () => {
+            this.$emit('beforeScroll')
+          })
+        }
+      },
+      enable() {
+        this.scroll && this.scroll.enable()
+      },
+      disable() {
+        this.scroll && this.scroll.disable()
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh()
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+      }
+    },
+    watch: {
+      data() {
+        setTimeout(() => {
+          this.refresh()
+        }, this.refreshDelay)
+      }
+    }
+  }
+</script>
+
